ef5116da67ab408bf75d3d55cfd6b00482b9b05c
Johannes Drechsler <jonjohnsonwright@gmail.com>
2017-03-24 14:59:46 +0100

    added FIleTemplates

---
+++ FileTemplates/vueSingleFileComponentTS.vue
@@ -0,0 +1,18 @@
+<template>
+	<section>
+		<h3>NAME-Component</h3>
+	</section>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+	import Component from 'vue-class-component'
+
+    @Component({
+		components: {
+		}
+	})
+	export default class NAME extends Vue {
+        
+	}
+</script>
